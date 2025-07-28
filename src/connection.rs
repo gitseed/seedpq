300879966b3061aad59181d842890ed0be5217d6
Paul Dejean <paulcdejean@gmail.com>
2025-07-27 22:10:52 -0500

    Fighting the borrow checking.

--- src/connection.rs
+++ src/connection.rs
@@ -1,13 +1,30 @@
 use std::sync::mpsc;
 
 use crate::libpq;
-pub struct Connection {
+
+/// The private struct containing the raw C pointer to the postgres connection.
+/// Has some implementations that apply to connections regardless of state.
+/// Most notably drop, which will call PQFinish on the connection.
+struct RawConnection {
     conn: *mut libpq::PGconn,
 }
 
+/// Represents an established connection, that is usable for doing things such as running queries.
+/// Every implementation on Connection will return a Result (or future Result) with FailedConnection as the error type.
+/// This is because a connection can go bad at any time and for any reason, as is the nature of computer networking.
+pub struct Connection {
+    conn: RawConnection,
+}
+
+/// A pending connection is not established or failed yet, but it impl Future, so it can be awaited to get a Result<Connection, BadConnection>
 pub struct PendingConnection {
-    conn: *mut libpq::PGconn,
+    conn: RawConnection,
     waker_send: std::sync::mpsc::Sender<Option<std::task::Waker>>,
+}
+
+/// A bad connection that can't be used for sending queries or similar.
+pub struct BadConnection {
+    conn: RawConnection,
 }
 
 #[derive(Debug)]
@@ -25,17 +42,14 @@ impl Connection {
         let conn: *mut libpq::pg_conn = unsafe { libpq::PQconnectStart(raw_conninfo) };
         let (waker_send, waker_receive) = mpsc::channel::<Option<std::task::Waker>>();
         wakeup_when_socket_writable(conn, waker_receive);
-        PendingConnection { conn, waker_send }
+        PendingConnection {
+            conn: RawConnection { conn },
+            waker_send,
+        }
     }
 
     pub fn server_version(&mut self) -> i64 {
-        (unsafe { libpq::PQserverVersion(self.conn) }) as i64
-    }
-}
-
-impl Drop for Connection {
-    fn drop(&mut self) {
-        unsafe { libpq::PQfinish(self.conn) }
+        (unsafe { libpq::PQserverVersion(self.conn.conn) }) as i64
     }
 }
 
@@ -46,13 +60,14 @@ impl Future for PendingConnection {
         self: std::pin::Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
     ) -> std::task::Poll<Self::Output> {
-        let status: libpq::PostgresPollingStatusType = unsafe { libpq::PQconnectPoll(self.conn) };
+        let status: libpq::PostgresPollingStatusType =
+            unsafe { libpq::PQconnectPoll(self.conn.conn) };
         if status == libpq::PostgresPollingStatusType::PGRES_POLLING_OK {
             self.waker_send.send(None).unwrap();
             std::task::Poll::Ready(Ok(Connection { conn: self.conn }))
         } else if status == libpq::PostgresPollingStatusType::PGRES_POLLING_FAILED {
             self.waker_send.send(None).unwrap();
-            std::task::Poll::Ready(Err(get_connection_error(self.conn)))
+            std::task::Poll::Ready(Err(get_connection_error(self.conn.conn)))
         } else {
             self.waker_send.send(Some(cx.waker().clone())).unwrap();
             std::task::Poll::Pending
