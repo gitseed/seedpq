fb08ca02bb2df30cc8fd389ec6c4fc8b1214e451
Paul Dejean <paulcdejean@gmail.com>
2025-07-28 01:53:38 -0500

    Better example.

--- examples/helloworld.rs
+++ examples/helloworld.rs
@@ -1,10 +1,11 @@
 use seedpq::connection::{BadConnection, GoodConnection, connect};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), BadConnection> {
     let c: Result<GoodConnection, BadConnection> = connect("postgres:///gitseed").await;
-    let (c, version) = c.unwrap().server_version();
+    let (c, version) = c?.server_version();
     println!("{}", version);
-    let (_, version) = c.unwrap().server_version();
+    let (_, version) = c?.server_version();
     println!("{}", version);
+    Ok(())
 }
