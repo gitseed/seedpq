c3b2d6b574c1c1354d5c833c06d6bd3115864c86
Paul Dejean <paulcdejean@gmail.com>
2025-08-23 00:14:21 -0500

    fmt

--- src/exec.rs
+++ src/exec.rs
@@ -3,7 +3,7 @@ use std::ptr::null;
 
 use crate::libpq;
 
-/// A pending query that can be awaited to obtain a Result<QueryResult, QueryError>. 
+/// A pending query that can be awaited to obtain a Result<QueryResult, QueryError>.
 pub struct PendingQuery<'a> {
     conn: &'a mut Connection,
 }
@@ -17,7 +17,9 @@ pub struct QueryResult {
 impl std::fmt::Display for QueryResult {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         let result = unsafe {
-            std::ffi::CStr::from_ptr(libpq::PQgetvalue(self.result, 0, 0)).to_str().unwrap()
+            std::ffi::CStr::from_ptr(libpq::PQgetvalue(self.result, 0, 0))
+                .to_str()
+                .unwrap()
         };
         write!(f, "{result}")
     }
@@ -30,7 +32,7 @@ impl Future for PendingQuery<'_> {
         mut self: std::pin::Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
     ) -> std::task::Poll<Self::Output> {
-        let raw_result: *mut libpq::pg_result = unsafe { libpq::PQgetResult(self.conn.raw() ) };
+        let raw_result: *mut libpq::pg_result = unsafe { libpq::PQgetResult(self.conn.raw()) };
         unsafe { libpq::PQgetResult(self.conn.raw()) };
         std::task::Poll::Ready(QueryResult { result: raw_result })
     }
@@ -42,19 +44,27 @@ impl Connection {
             return Err(self.error());
         }
 
-        let ffi_query: std::ffi::CString = std::ffi::CString::new(query).expect("postgres queries can not contain null characters");
+        let ffi_query: std::ffi::CString = std::ffi::CString::new(query)
+            .expect("postgres queries can not contain null characters");
 
         let sent_successfully: std::ffi::c_int = unsafe {
-            libpq::PQsendQueryParams(self.raw(), ffi_query.as_ptr(), 0, null(), null(), null(), null(), 0)
+            libpq::PQsendQueryParams(
+                self.raw(),
+                ffi_query.as_ptr(),
+                0,
+                null(),
+                null(),
+                null(),
+                null(),
+                0,
+            )
         };
 
         // From the docs: "1 is returned if the command was successfully dispatched and 0 if not"
         if sent_successfully != 1 {
             Err(self.error())
         } else {
-            Ok(PendingQuery {
-                conn: self
-            })
+            Ok(PendingQuery { conn: self })
         }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! Library entry point for seedpq. Just pub mod statements for now.
 
 pub mod connection;
+pub mod exec;
 pub mod libpq;
 pub mod status;
-pub mod exec;
