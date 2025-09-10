d2ce517eb616dae26c0fdace1277a671f09f6441
Paul Dejean <paulcdejean@gmail.com>
2025-09-10 17:38:20 -0500

    Connection per bench.

--- benches/baseline.rs
+++ benches/baseline.rs
@@ -3,13 +3,19 @@ use seedpq::libpq;
 pub struct Connection(*mut libpq::PGconn);
 impl Drop for Connection {
     fn drop(&mut self) {
-        unsafe { libpq::PQfinish(self.0) }
+        // unsafe { libpq::PQfinish(self.0) }
     }
 }
-pub fn bench_trivial_seed(b: &mut Bencher) {
-    let c: Connection = Connection(unsafe { libpq::PQconnectdb(c"postgres:///example".as_ptr()) });
-    b.iter(|| {
-        unsafe {
+
+fn bench_trivial_seed(b: &mut Bencher) {
+    b.iter_batched(
+        || {
+            let c: Connection =
+                Connection(unsafe { libpq::PQconnectdb(c"postgres:///example".as_ptr()) });
+            std::hint::black_box(&c);
+            c
+        },
+        |c| unsafe {
             let result: *mut libpq::pg_result = libpq::PQexecParams(
                 c.0,
                 c"SELECT id, name, hair_color FROM users".as_ptr(),
@@ -21,10 +27,11 @@ pub fn bench_trivial_seed(b: &mut Bencher) {
                 1,
             );
             libpq::PQclear(result);
-        };
-    });
-    std::hint::black_box(c);
+        },
+        criterion::BatchSize::SmallInput,
+    )
 }
+
 fn bench_trivial_query(c: &mut Criterion) {
     let mut group = c.benchmark_group("bench_trivial_query");
     group.bench_function("baseline", bench_trivial_seed);
