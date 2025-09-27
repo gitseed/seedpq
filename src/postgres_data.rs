d5d972236d62bf41ba2c38efaca4446f300cfc39
Paul Dejean <paulcdejean@gmail.com>
2025-09-27 18:05:57 -0500

    nm this was a useless impl

--- src/postgres_data.rs
+++ src/postgres_data.rs
@@ -1,8 +1,2 @@
 #[derive(Copy, Clone, Debug)]
 pub struct PostgresData<'a>(pub Option<&'a [u8]>);
-
-impl<'a> Into<Option<&'a [u8]>> for PostgresData<'a> {
-    fn into(self) -> Option<&'a [u8]> {
-        self.0
-    }
-}
