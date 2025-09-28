ec47d70da77863fe4338f75eac455530c50f18ea
Paul Dejean <paulcdejean@gmail.com>
2025-09-28 00:58:48 -0500

    That's all for today!

--- examples/timestamp.rs
+++ examples/timestamp.rs
@@ -1,16 +1,18 @@
+
 fn _main() -> Result<(), Box<dyn std::error::Error>> {
     let (db_send, db_recv, _, _) = seedpq::connect("postgres:///gitseed");
-    db_send.exec("truncate table timestamp cascade")?;
-    db_send.exec("insert into timestamp(epoch)")?;
-    db_send.exec("insert into timestamp(infinity)")?;
-    db_send.exec("insert into timestamp(-infinity)")?;
-    db_send.exec("insert into timestamp(now)")?;
-    // db_send.exec("select * from timestamp")?;
+    db_send.exec("truncate table timestamps cascade")?;
+    db_send.exec("insert into timestamps (ts) values ('infinity');")?;
+    db_send.exec("insert into timestamps (ts) values ('-infinity');")?;
     db_recv.get()?.none()?;
     db_recv.get()?.none()?;
     db_recv.get()?.none()?;
-    db_recv.get()?.none()?;
-    db_recv.get()?.none()?;
+    db_send.exec("select * from timestamps")?;
+    let mut result: seedpq::QueryReceiver<i64> = db_recv.get()?;
+    let pg_inf: i64 = result.next().unwrap()?;
+    assert_eq!(pg_inf, i64::MAX);
+    let pg_neginf: i64 = result.next().unwrap()?;
+    assert_eq!(pg_neginf, i64::MIN);
     Ok(())
 }
 
